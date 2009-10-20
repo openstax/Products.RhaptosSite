295eea39d392b3515b9dacc3ea41d2545920cf77
Josh LaPlace <josh@cnx.rice.edu>
2009-10-20 17:30:14 +0000

    change example email

--- Products/RhaptosSite/setup/RhaptosSetup.py
+++ Products/RhaptosSite/setup/RhaptosSetup.py
@@ -547,7 +547,7 @@ def createHelpSection(self, portal):
                 text = mod_regx.sub('"http://cnx.org\g<1>"', text)
             email_match = email_regx.search(text)
             if email_match:
-                text = email_regx.sub('changeme@example.com', text)
+                text = email_regx.sub('changeme@example.org', text)
             if mod_match or col_match or email_match or cnxorg_match:
                 doc.setText(text, mimetype=doc.getContentType())
 
