b5b1f96951f4665b82624c9fa40fb385236c27ca
Philip Schatz <phil@cnx.org>
2010-08-30 21:35:13 +0000

    Merging from trunk

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.31.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = read("Products", "RhaptosSite", "version.txt").strip()
 
 setup(name='Products.RhaptosSite',
       version=version,
