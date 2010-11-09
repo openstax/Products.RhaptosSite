6ce3a9aef2a4b4856e8f0ff4116fe2b8bec56a5d
Roche Compaan <roche@cnx.rice.edu>
2010-11-09 13:58:00 +0000

    removed imports of classes that no longer exist. RhaptosSite is not really installable at the moment and needs to be refactored before it will work again.

--- Products/RhaptosSite/RhaptosSite.py
+++ Products/RhaptosSite/RhaptosSite.py
@@ -1,23 +1,18 @@
 import os, sys, re
-from __future__ import nested_scopes
 
 from zope.interface import implements
 
 import Globals
 from ComputedAttribute import ComputedAttribute
 from Products.CMFPlone import cmfplone_globals
-from Products.CMFPlone import custom_policies
-from Products.CMFPlone import ToolNames
 from Products.CMFDefault.Portal import CMFSite
 from Products.CMFPlone.Portal import PloneSite
-from Products.CMFPlone.setuphandlers import PloneGenerator
 from Products.CMFCore import permissions
 from Products.CMFCore.TypesTool import FactoryTypeInformation
 from Products.CMFCore.DirectoryView import addDirectoryViews
 from Products.CMFCore.utils import getToolByName
 from Products.CMFDefault import Portal, DublinCore
 from Products.CMFPlone.PloneFolder import OrderedContainer
-from Products.CMFDefault.Portal import manage_addCMFSite
 #from Products.CMFPlone.Portal import manage_addSite   # jcc TODO breaks install!
 from Products.CMFPlone.factory import addPloneSite
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
@@ -33,8 +28,6 @@ class RhaptosSite(PloneSite, CMFSite, OrderedContainer):
     """
     security=ClassSecurityInfo()
     meta_type = portal_type = 'Rhaptos Site'
-    __implements__ = DublinCore.DefaultDublinCoreImpl.__implements__ + \
-                     OrderedContainer.__implements__
 
 Globals.InitializeClass(RhaptosSite)
 
@@ -77,7 +70,7 @@ def register(context, globals):
                           constructors=(manage_addSiteForm,
                                         manage_addRhaptosSite,) )
 
-class RhaptosGenerator(PloneGenerator):
+class RhaptosGenerator():
 
     def create(self, parent, id, create_userfolder):
         plone = PloneGenerator().create(parent, id, create_userfolder)
