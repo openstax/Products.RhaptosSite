7fea9781b4caaba406716bb265dc8241452246f6
Michael Mulich <michael.mulich@gmail.com>
2011-03-08 18:12:47 +0000

    Enables field validation for our custom fields. There is however a problem with the site license agreement checkbox that will need to be addressed in the future. See the code's FIXME comment for details.

--- Products/RhaptosSite/browser/register.py
+++ Products/RhaptosSite/browser/register.py
@@ -9,6 +9,7 @@ Rhaptos notes
 from zope.interface import Interface
 from zope import schema
 from zope.formlib import form
+from zope.formlib.interfaces import WidgetInputError
 from plone.app.users.browser import register
 
 from Products.RhaptosSite import RhaptosMessageFactory as _
@@ -21,17 +22,21 @@ class IRhaptosRegistrationSchema(Interface):
     #       two separate fields: first name and last name? 
     ##first_name = 
     ##last_name = 
-    home_page = schema.ASCIILine(
-        title=_(u'lable_user_home_page', default=u'Home Page'),
+    home_page = schema.URI(
+        title=_(u'lable_user_home_page', default=u'Home page'),
         description=_(u'help_user_home_page',
                       default=u"Enter the address of your personal Web "
-                               "page e.g. http://www.jdoe.com/~jdoe/"),
-        required=False,)
+                              "page e.g. http://www.jdoe.com/~jdoe/"),
+        required=False,
+        )
     site_license_agreement = schema.Bool(
         title=_(u'site_license_agreement',
-                # TODO: Link to the Connexions Site License
-                default=u"I have read the Connexions Site License and I agree to be bound by its terms"),
-        default=False)
+                # FIXME: Hard-coded value to a site license that doesn't exist.
+                default=u"I have read the <a href=\"/sitelicense\">"
+                        "Connexions Site License</a> and I "
+                        "agree to be bound by its terms"),
+        required=True,
+        )
 
 
 class RegistrationForm(register.RegistrationForm):
@@ -46,6 +51,29 @@ class RegistrationForm(register.RegistrationForm):
         #: Add our fields to the mix.
         form_fields += form.Fields(IRhaptosRegistrationSchema)
         return form_fields
+
+    # Actions validators
+    def validate_registration(self, action, data):
+        """Specific business logic for this registration (join) form."""
+        errors = super(RegistrationForm, self).validate_registration(action,
+                                                                     data)
+        # Note: Validation for the home page field is done in the super class.
+
+        #: Validate the site license agreement field.
+        # FIXME: Currently this error does not display correctly. The status
+        #        message will appear at the top of the page but the field will
+        #        not highlight. 
+        site_license_agreement = self.widgets['site_license_agreement']
+        if site_license_agreement.getInputValue() is not True:
+            err_str = _('msg_has_not_agreed_to_site_license',
+                        default=u"You must agree to the terms of service "
+                                "in the Connexions site license.")
+            #: Note: CheckBoxWidget widget doesn't have a label for stylistic
+            #  reasons, but we need it for the form validation.
+            errors.append(WidgetInputError(site_license_agreement.name,
+                                           site_license_agreement.name,
+                                           err_str))
+        return errors
 
 
 class AddUserForm(register.BaseRegistrationForm):
