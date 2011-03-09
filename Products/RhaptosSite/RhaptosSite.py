ed716a97a884e61d07e1214fabac4abf356305e1
Michael Mulich <michael.mulich@gmail.com>
2011-03-09 19:38:15 +0000

    Redirect to the new constructor.

--- Products/RhaptosSite/RhaptosSite.py
+++ Products/RhaptosSite/RhaptosSite.py
@@ -1,46 +1,15 @@
 # -*- coding: utf-8 -*-
-from Products.CMFPlone.factory import addPloneSite
-from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from AccessControl.Permissions import view_management_screens
 
 
-manage_addSiteForm = PageTemplateFile('www/addRhaptosSite', globals())
-manage_addSiteForm.__name__ = 'addRhaptosSite'
-def manage_addRhaptosSite(self, id, title='', description='',
-                          create_userfolder=1,
-                          email_from_address='',
-                          email_from_name='Rhaptos',
-                          validate_email=0,
-                          dbauser='rhaptos_dba',
-                          dbapass=None,
-                          dbuser='rhaptos',
-                          dbpass=None,
-                          dbname='repository',
-                          dbserver=None,
-                          dbport=None,
-                          RESPONSE=None):
-    """ Rhaptos Site factory """
-    temp_id = 'DB_OPTS_TEMP'
-    if not hasattr(self.aq_parent.aq_parent,temp_id):
-        self.aq_parent.aq_parent.manage_addFolder(temp_id)
-    root = self.aq_parent.aq_parent[temp_id]
-    root._dbopts={}
-    root._dbopts['admin']=dbauser
-    root._dbopts['adminpass']=dbapass
-    root._dbopts['user']=dbuser
-    root._dbopts['userpass']=dbpass
-    root._dbopts['dbname']=dbname
-    root._dbopts['server']=dbserver
-    root._dbopts['port']=dbport
-
-    site = addPloneSite(self, id, title, description, create_userfolder,
-                        email_from_address, email_from_name, validate_email,
-                        extension_ids=('Products.RhaptosSite:default',))
-
-    RESPONSE.redirect('%s/manage_main' % site.absolute_url())
+def zmi_constructor(context):
+    """This is a dummy constructor for the ZMI."""
+    url = context.DestinationURL()
+    request = context.REQUEST
+    return request.response.redirect(url + '/@@rhaptos-addsite?site_id=Rhaptos')
 
 
 def register(context, globals):
     context.registerClass(meta_type='Rhaptos Site',
-                          permission='Add CMF Sites',
-                          constructors=(manage_addSiteForm,
-                                        manage_addRhaptosSite,) )
+                          permission=view_management_screens,
+                          constructors=(zmi_constructor,))
