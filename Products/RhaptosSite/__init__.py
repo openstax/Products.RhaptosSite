cb4c65c41a77d4c6b17e0b87a08d620ff8ac5f2e
Michael Mulich <michael.mulich@gmail.com>
2011-03-09 20:07:20 +0000

    Removal of dead imports; moving of lines for python stylistic reasons and readability.

--- Products/RhaptosSite/__init__.py
+++ Products/RhaptosSite/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """ 
 Rhaptos Initialization
 
@@ -7,17 +8,19 @@ Author: Brent Hendricks
 This software is subject to the provisions of the GNU General
 Public License Version 2 (GPL).  See LICENSE.txt for details.
 """
-
-# We define this first so that RhaptosSetup can import it w/o recursion
+#: We define this first so that RhaptosSetup can import it w/o recursion
 product_globals = globals()
 
 import sys
-from Products.CMFCore.DirectoryView import registerDirectory
-from Products.CMFCore import utils
+from AccessControl import allow_module
 
-from Products.GenericSetup import BASE, EXTENSION
-from Products.GenericSetup import profile_registry
-from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
+from Products.CMFCore.DirectoryView import registerFileExtension
+from Products.CMFCore.FSFile import FSFile
+
+from Products.PluggableAuthService import registerMultiPlugin
+from Products.PluggableAuthService.permissions import ManageUsers
+
+from Products.RhaptosSite.pas import AltHashAuth as AHA
 
 # TODO: Review the following imports - they are likely not necessary
 from Products.RhaptosSite import RhaptosSite
@@ -25,39 +28,15 @@ from Products.RhaptosSite import monkeypatch
 from Products.RhaptosSite import managercatalog  # mostly to test syntax
 from Products.RhaptosSite import security
 
-this_module = sys.modules[ __name__ ]
 
-
-# we have an EPS in our skins, and must teach FSDV how to treat it
-from Products.CMFCore.DirectoryView import registerFileExtension
-from Products.CMFCore.FSFile import FSFile
-registerFileExtension('eps', FSFile)
-
-from Products.PluggableAuthService import registerMultiPlugin
-from Products.PluggableAuthService.permissions import ManageUsers
-import pas.AltHashAuth as AHA
-from AccessControl import allow_module
-
+this_module = sys.modules[__name__]
 registerMultiPlugin(AHA.AltHashAuth.meta_type)
-
-
-def initialize(context):
-    # BBB: Keep for ZMI add drop-down support. (2011-03-09)
-    RhaptosSite.register(context, product_globals)
-
-    context.registerClass( AHA.AltHashAuth
-                         , permission=ManageUsers
-                         , constructors=(
-                            AHA.manage_addAltHashAuthForm,
-                            AHA.addAltHashAuth, )
-                         , visibility=None
-                         , icon='pas/www/altauth.png'
-                         )
+#: We have an EPS in our skins, and must teach FSDV how to treat it
+registerFileExtension('eps', FSFile)
 
 # Import "RhaptosMessageFactory as _" to create message ids in the rhaptos domain
 # Zope 3.1-style messagefactory module
 # BBB: Zope 2.8 / Zope X3.0
-
 allow_module('Products.RhaptosSite.messagefactory_')
 try:
     from zope.i18nmessageid import MessageFactory
@@ -65,3 +44,16 @@ except ImportError:
     from messagefactory_ import RhaptosMessageFactory
 else:
     RhaptosMessageFactory = MessageFactory('rhaptos')
+
+
+def initialize(context):
+    # BBB: Keep for ZMI add drop-down support. (2011-03-09)
+    RhaptosSite.register(context, product_globals)
+
+    context.registerClass(AHA.AltHashAuth,
+                          permission=ManageUsers,
+                          constructors=(AHA.manage_addAltHashAuthForm,
+                                        AHA.addAltHashAuth,),
+                          visibility=None,
+                          icon='pas/www/altauth.png',
+                          )
