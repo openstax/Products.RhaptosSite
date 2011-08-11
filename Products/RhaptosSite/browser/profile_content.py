6566ebd63d2f65dbb88151ce645e78d29dbdd6f9
David Bain <david@cnx.rice.edu>
2011-08-11 14:35:30 +0000

    created a custom view for profile_content; created the portlet for the member profile and started to layout the new member_profile view

---
+++ Products/RhaptosSite/browser/profile_content.py
@@ -0,0 +1,49 @@
+from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+from Acquisition import aq_inner
+
+class ProfileContent(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    @property
+    def content_catalog(self):
+        context = aq_inner(self.context)
+        return getToolByName(context,'content').catalog
+
+    def getAuthCollectionsForMember(self,memberid):
+        query = {}
+        query['portal_type'] = ['Collection',]
+        query['authors'] = memberid
+        query['sort_on'] = 'modified'
+        query['sort_order'] = 'reverse'
+        return self.content_catalog(query)
+
+    def getAuthModulesForMember(self,memberid):
+        query = {}
+        query['portal_type'] = ['Module',]
+        query['authors'] = memberid
+        query['sort_on'] = 'modified'
+        query['sort_order'] = 'reverse'
+        return self.content_catalog(query)
+
+    def getMaintainedModulesForMember(self,memberid):
+        query = {}
+        query['portal_type'] = ['Module',]
+        query['maintainers'] = memberid
+        query['sort_on'] = 'modified'
+        query['sort_order'] = 'reverse'
+        return self.content_catalog(query)
+
+
+
+    def getTranslationsForMember(self,memberid):
+        query = {}
+        query['portal_type'] = ['Module',]
+        query['sort_on'] = 'modified'
+        query['sort_order'] = 'reverse'
+        return self.content_catalog(query)
+
+
